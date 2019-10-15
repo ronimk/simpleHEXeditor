267462e4348819c1d340cbfa2e2f0ae702fdc796
ronimk <ronimk@users.noreply.github.com>
2019-10-15 22:11:59 +0300

    Create hex_editor.h

---
+++ src/hex_editor.h
@@ -0,0 +1,20 @@
+#ifndef HEX_EDITOR_H_
+#define HEX_EDITOR_H_
+
+#include <stdio.h>
+#include "file_handler.h"
+
+#define NUM_CMDS 10
+#define MAX_CMD_NAME_LEN 9
+
+typedef enum description_mode {brief, detailed} descr_mode;
+
+typedef struct command {
+    char *name;
+    void (*eval_f)(const char *arg_str, file_handler *open_file);
+    void (*description)(descr_mode m);
+} command;
+
+void init_commands(void);
+
+#endif /* HEX_EDITOR_H_ */
