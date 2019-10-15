cfa8c3c05483603049efa18a48489a16ab850d47
ronimk <ronimk@users.noreply.github.com>
2019-10-15 22:13:53 +0300

    Create descriptions.h

---
+++ src/descriptions.h
@@ -0,0 +1,17 @@
+#ifndef DESCRIPTIONS_H_
+#define DESCRIPTIONS_H_
+
+#include "hex_editor.h"
+
+void print_more_info(descr_mode m);
+void print_show_info(descr_mode m);
+void print_del_info(descr_mode m);
+void print_add_info(descr_mode m);
+void print_mod_info(descr_mode m);
+void print_filesize_info(descr_mode m);
+void print_save_info(descr_mode m);
+void print_load_info(descr_mode m);
+void print_apply_info(descr_mode m);
+void print_quit_info(descr_mode m);
+
+#endif /* DESCRIPTIONS_H_ */
