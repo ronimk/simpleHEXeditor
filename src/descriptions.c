abd5f3be5977ca40eb653b60fbc50ef7d08b9e73
ronimk <roni.kekkonen@gmail.com>
2019-10-20 12:35:34 +0300

    cleaned up descriptions

--- src/descriptions.c
+++ src/descriptions.c
@@ -19,12 +19,13 @@ void print_show_info(descr_mode m)
     if (m == detailed)
     {
         printf("\n");
-        printf("[start] and [end] are integer values denoting the starting and ending positions of a modifiable block of bytes.\n");
-        printf("They both must be within the accepted limits of the file: from 0 to the size of the file minus one.\n");
+        printf("[start] and [end] are either decimal or hexadecimal (prepended with \"0X\") values denoting the starting\n");
+        printf("and ending positions of a modifiable block of bytes. They both must be within the accepted limits of the\n");
+        printf("file: from 0 to one less than the size of the file.\n");
         printf("\n");
         printf("Examples:\n");
         printf("\t\"show 32 63\": outputs all the bytes of opened file from the 32nd byte of the file up to the 63rd byte.\n");
-        printf("\t\"show 0 11\": shows the first 12 bytes in the file.\n");
+        printf("\t\"show 0x0 0xB\": shows the first 13 bytes in the file.\n");
         printf("\n");
         printf("NOTE: There must exist a currently open file, or the command produces and error.\n");
     }
@@ -36,12 +37,13 @@ void print_del_info(descr_mode m)
     if (m == detailed)
     {
         printf("\n");
-        printf("[start] and [end] are integer values denoting the starting and ending positions of the byte-block to be deleted.\n");
-        printf("They both must be within the accepted limits of the file: from 0 to the size of the file minues one.\n");
+        printf("[start] and [end] are either decimal or hexadecimal (prepended with \"0X\") values denoting the starting\n");
+        printf("and ending positions of the byte-block to be deleted. They both must be within the accepted limits of the\n");
+        printf("file: from 0 to one less than the size of the file.\n");
         printf("\n");
         printf("Examples:\n");
-        printf("\t\"del 32 62\": deletes all the bytes of the opened file from the 32nd byte of the file up to the 63rd byte.\n");
-        printf("\t\"del 0 11\": deletes the first 12 bytes in the file.\n");
+        printf("\t\"del 32 62\": deletes all the bytes of the opened file from the 33rd byte of the file up to the 63rd byte.\n");
+        printf("\t\"del 0x0 0x11\": deletes the first 18 bytes in the file.\n");
         printf("\n");
         printf("NOTE: There must exist a currently open file, or the command produces and error.\n");
     }
@@ -53,10 +55,16 @@ void print_add_info(descr_mode m)
     if (m == detailed)
     {
         printf("\n");
-        printf("[start] is an integer value denoting the position where the new bytes should be added.\n");
-        printf("It must be within the accepted limits of the file: from 0 to the size of the file minus one.\n");
+        printf("[start] is  either a decimal or a hexadecimal (prepended with \"0X\") value denoting the position where the\n");
+        printf("new bytes should be added. It must be within a range from 0 to the size of the file. When [start] equals\n");
+        printf("filesize, the new bytes will be added as to the end of the file as the last bytes in the file.\n");
         printf("\n");
-        printf("[hex+] is a non-empty stream of hexadecimal data.\n\tEx. 1: A9B65FF7\n\tEx. 2: 000ff95b\n");
+        printf("[hex+] is a non-empty stream of hexadecimal data.\n");
+        printf("\n");
+        printf("Examples:\n");
+        printf("\t\"add 0x4A 0FA1CC\": adds the bytes 0FA1CC into the 75th byte position, moving all upper\n");
+        printf("\t                   bytes three spaces to make room for the new bytes.\n");
+        printf("\t\"add 0 C356ABD451FF00\"\n");
         printf("\n");
         printf("NOTE: There must exist a currently open file, or the command produces and error.\n");
     }
@@ -68,12 +76,18 @@ void print_mod_info(descr_mode m)
     if (m == detailed)
     {
         printf("\n");
-        printf("[start] and [end] are integer values denoting the starting and ending positions of a modifiable block of bytes.\n");
-        printf("They both must be within the accepted limits of the file: from 0 to the size of the file minus one.\n");
+        printf("[start] and [end] are either decimal or hexadecimal (prepended with \"0X\") values denoting the starting\n");
+        printf("and ending positions of a modifiable block of bytes. They both must be within the accepted limits of the file:\n");
+        printf("from 0 to one less than the size of the file.\n");
         printf("\n");
-        printf("[hex+] is a non-empty stream of hexadecimal data.\n\tEx. 1: A9B65FF7\n\tEx. 2: 000ff95b\n\n");
-        printf("The change is performed to the entire blocks of bytes. Thus the size of [hex+] block\n");
-        printf("does not have to match [end]-[start]+1.\n");
+        printf("[hex+] is a non-empty stream of hexadecimal data. The change is performed to the entire blocks of bytes.\n");
+        printf("Thus the size of [hex+] block does not have to match [end]-[start]+1.\n");
+        printf("\n");
+        printf("Examples:\n");
+        printf("\t\"mod 0x4A 0x8C 0FA1CC\": first deletes the block starting from byte position 4A and ending into\n");
+        printf("\t                        byte position 8C, then adds the bytes 0FA1CC into the byte position 4A.\n");
+        printf("\t\"mod 0 2 C356ABFF00\":   deletes the first three bytes and injects the bytes C356ABFF00 to\n");
+        printf("\t                        the beginning of the file.");
         printf("\n");
         printf("NOTE: There must exist a currently open file, or the command produces and error.\n");
     }
@@ -120,11 +134,12 @@ void print_apply_info(descr_mode m)
     {
         printf("\n");
         printf("[patch]: the name of the patch file.\n");
-        printf("If \"reverse\" is given as a second argument to apply, the patch is applied so that the file\n");
-        printf("will be reverted back to an earlier state, before all the patch modifications to the file.\n");
-        printf("Otherwise the commands in the patch are applied directly.\n");
+        printf("If \"reverse\" is given as a second argument to apply, the patch is applied so that the file will be reverted\n");
+        printf("back to an earlier, unmodified version. Otherwise the commands in the patch are applied directly, as if\n");
+        printf("they were typed one by one into the console.\n");
         printf("\n");
-        printf("The file must not contain inconsistent data relative to the patch or the file will become corrupted.\n");
+        printf("The file should not contain inconsistent data relative to the patch or the file will become corrupted.\n");
+        printf("It is left as the user's responsibility to ensure the validity of the patchfile to be applied.\n");
         printf("\n");
         printf("NOTE: There must exist a currently opened file, or the command produces and error.\n");
     }
